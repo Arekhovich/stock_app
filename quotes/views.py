16ec9a3628f80fe9e53ded964e6a03c453c1c331
Arekhovich <e.orechovich@tut.by>
2020-12-08 16:57:06 +0300

    Add page for add stock and page for delete stock

--- quotes/views.py
+++ quotes/views.py
@@ -1,21 +1,65 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import StockForm
+
+from quotes.models import Stock
+
 
 def home(request):
     import requests
     import json
 
-    # pk_80c91974cc9f4db58f64b11769d7ef03
-    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_80c91974cc9f4db58f64b11769d7ef03')
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        # pk_80c91974cc9f4db58f64b11769d7ef03
+        api_request = requests.get('https://cloud.iexapis.com/stable/stock/'
+                                   + ticker + '/quote?token=pk_80c91974cc9f4db58f64b11769d7ef03')
+        try:
+            api = json.loads(api_request.content)
+        except Exception:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': 'Enter a Ticker Symbol Above'})
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error ..."
-
-
-
-
-    return  render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def add_stock(request):
+    import requests
+    import json
+
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Stock has been added')
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get('https://cloud.iexapis.com/stable/stock/'
+                                       + str(ticker_item) + '/quote?token=pk_80c91974cc9f4db58f64b11769d7ef03')
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception:
+                api = 'Error...'
+
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
+
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, 'Stock has been deleted')
+    return redirect(delete_stock)
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, 'delete_stock.html', {'ticker': ticker})
+
